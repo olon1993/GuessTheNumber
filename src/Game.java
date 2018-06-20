1fe946ad4a7099d3ff151d0b1587b21f72404761
Scott Batey <sebateyjr@yahoo.com>
2018-06-20 00:59:14 -0400

    Added a javafx gui to the game.

--- src/Game.java
+++ src/Game.java
@@ -1,101 +1,140 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 import java.util.Scanner;
 
-public class Game {
+public class Game extends Application{
 
-	//Game Variables
-	private int low = 1,
-				high = 1000,
-				mid = low + (high - low) / 2,
-				guesses = 0;
+	// Game Options
+	private final String GAMEOVER = "I win! I guessed your number in ";
+	private final int HEIGHT = 120;
+	private final int WIDTH = 300;
+	private final int newLow = 1,
+					  newHigh = 1000;
 	
-	private Scanner keyboard;
+	// Game Variables
+	private int low;
+	private int high;
+	private int	mid,
+				guesses;
 	
-	public Game() {
-		init();
-		gameLoop();
+	// Scene Variables
+	private TextArea gameText;
+	private Text score;
+	private Button yesButton,
+				   noButton,
+				   higherButton,
+				   lowerButton,
+				   newGameButton;
+	
+	public void start(Stage s) throws Exception {
+		sceneInit(s);
+		gameInit();
 	}
 	
 	public static void main(String[] args) {
-		new Game();
+		launch(args);
 	}
 	
-	public void init() {
-		keyboard = new Scanner(System.in);
+	public void sceneInit(Stage stage) {
+		stage = new Stage();
+		stage.setTitle("Number Guess");
 		
-		while(true) {
-			//Allow the player to pick a number
-			System.out.println("Pick a number between " + low + " and " + high + ".");
-			System.out.print("Ready? ");
-			String response = keyboard.nextLine();
-			
-			if(response.trim().toUpperCase().contentEquals("YES") || 
-			   response.trim().toUpperCase().contentEquals("Y")) {
-				break;
-			} else if(response.trim().toUpperCase().contentEquals("NO") || 
-					  response.trim().toUpperCase().contentEquals("N")) {
-				System.out.println("Take your time. When you have decided upon a number "
-						+ "type \"Yes\" or \"Y\".");
-			} else {
-				System.out.println("Type \"Yes\" or \"Y\" when you have decided upon a "
-						+ "number. If you need more time type \"No\" or \"N\".");
-			}
-		}
+		gameText = new TextArea();
+		gameText.setMinSize(150, 75);
+		gameText.setWrapText(true);
+		gameText.setEditable(false);
+		
+		score = new Text();
+		
+		newGameButton = new Button("New Game");
+		newGameButton.setOnAction(e -> { gameInit(); } );
+		
+		yesButton = new Button("Yes");
+		yesButton.setOnAction(e -> { gameOver(); } );
+		
+		noButton = new Button("No");
+		noButton.setOnAction(e -> { incorrectGuess(); } );
+		
+		higherButton = new Button("Higher");
+		higherButton.setOnAction(e -> { newGuess(mid, high); } );
+		
+		lowerButton = new Button("Lower");
+		lowerButton.setOnAction(e -> { newGuess(low, mid); } );
+		
+		yesButton.setMinSize(75, 30);
+		noButton.setMinSize(75, 30);
+		higherButton.setMinSize(75, 30);
+		lowerButton.setMinSize(75, 30);
+		newGameButton.setMinSize(150, 30);
+		
+		HBox root = new HBox();
+		VBox controlBox = new VBox();
+		HBox yesnoBox = new HBox();
+		HBox highlowBox = new HBox();
+		
+		yesnoBox.getChildren().addAll(yesButton, noButton);
+		highlowBox.getChildren().addAll(higherButton, lowerButton);
+		controlBox.getChildren().addAll(score, yesnoBox, highlowBox, newGameButton);
+		root.getChildren().addAll(gameText, controlBox);
+		
+		yesnoBox.setAlignment(Pos.CENTER);
+		highlowBox.setAlignment(Pos.CENTER);
+		controlBox.setAlignment(Pos.CENTER);
+		
+		Scene scene = new Scene(root, WIDTH, HEIGHT);
+		stage.setScene(scene);
+		stage.show();
 	}
 	
-	public void gameLoop() {
-		// The computer uses an algorithm similar to binary search to narrow down 
-		// the possibilities before ultimately guessing the correct number.
-		while(true) {
-			System.out.println("Is your number " + mid + "?");
-			String response = keyboard.nextLine();
-			
-			// The game is over, the computer guessed the correct number
-			if(response.trim().toUpperCase().contentEquals("YES") || 
-			   response.trim().toUpperCase().contentEquals("Y")) {
-				guesses++;
-				System.out.println("I guessed your number in " + guesses + " guesses!");
-				break;
-				
-			// The computer guessed wrong and must guess again
-			} else if(response.trim().toUpperCase().contentEquals("NO") || 
-					  response.trim().toUpperCase().contentEquals("N")){
-				guesses++;
-				
-				while(true) {
-					System.out.println("Is your number higher or lower than " + mid + "? ");
-					response = keyboard.nextLine();
-				
-					if(response.trim().toUpperCase().contentEquals("HIGHER") || 
-					   response.trim().toUpperCase().contentEquals("H")) {
-						newGuess(mid, high);
-						break;
-					} else if(response.trim().toUpperCase().contentEquals("LOWER") || 
-							  response.trim().toUpperCase().contentEquals("L")){
-						newGuess(low, mid);
-						break;
-						
-					// Invalid input, the player is reminded of the rules of the game.
-					} else {
-						System.out.println("I'm sorry, I didn't quite undterstand that. If your "
-								+ "number is higher than " + mid + " type \"Higher\" or \"H\". "
-								+ "If your number is lower than " + mid + " type \"Lower\" or "
-								+ "\"L\".");
-					}
-				}
-			
-			// Invalid input, the player is reminded of the rules of the game.
-			} else {
-				System.out.println("I'm sorry, I didn't quite undterstand that. If I "
-						+ "guessed your number correctly type \"Yes\" or \"Y\". If I "
-						+ "guessed your number incorrectly type \"No\" or \"N\".");
-			}
-		}
+	public void gameInit() {
+		low = newLow;
+		high = newHigh;
+		mid = low + (high - low) / 2;
+		guesses = 1;
+		score.setText("Score: 0");
+		yesButton.setDisable(false);
+		noButton.setDisable(false);
+		higherButton.setDisable(true);
+		lowerButton.setDisable(true);
+		gameText.setText("Pick a number between " + low + " and " + high + ". "
+					   + "Is your number " + mid + "?");
+	}
+	
+	public void gameOver() {
+		yesButton.setDisable(true);
+		noButton.setDisable(true);
+		higherButton.setDisable(true);
+		lowerButton.setDisable(true);
+		gameText.setText(GAMEOVER + guesses + " guesses!");
+	}
+	
+	public void incorrectGuess() {
+		score.setText("Score: " + guesses);
+		gameText.setText("Is your number higher or lower than " + mid + "? ");
+		yesButton.setDisable(true);
+		noButton.setDisable(true);
+		higherButton.setDisable(false);
+		lowerButton.setDisable(false);
 	}
 	
 	public void newGuess(int low, int high) {
 		this.low = low;
 		this.high = high;
 		this.mid = low + (high - low) / 2;
+		guesses++;
+		gameText.setText("Is your number " + mid + "?");
+		yesButton.setDisable(false);
+		noButton.setDisable(false);
+		higherButton.setDisable(true);
+		lowerButton.setDisable(true);
 	}
 	
 }
